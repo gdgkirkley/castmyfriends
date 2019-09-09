5a29a5e913153082c353ab56c2b5af59b28d6a50
Gabriel Kirkley <gdgkirkley@gmail.com>
2019-09-08 18:16:59 -0700

    First deploy

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { auth } from "../firebase/firebase.utils";
 
 const FooterStyles = styled.div`
